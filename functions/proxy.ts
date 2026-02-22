e839efd26478d559d1b160db17834028fdd05a4b
yukiiiiiiiiiiiiiiiiiiiiiii <139841171+yukiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2026-02-22 15:53:18 +0800

    Change API base URL to local server

--- functions/proxy.ts
+++ functions/proxy.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://music-api.gdstudio.xyz/api.php";
+const API_BASE_URL = "http://192.168.188.2:4321";
 const KUWO_HOST_PATTERN = /(^|\.)kuwo\.cn$/i;
 const SAFE_RESPONSE_HEADERS = ["content-type", "cache-control", "accept-ranges", "content-length", "content-range", "etag", "last-modified", "expires"];
 
